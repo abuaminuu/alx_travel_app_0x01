eb6e3b00f7674674a0d9be665ae996d4cc9eefd1
abuaminuu <i.abuaminu@gmail.com>
2025-11-27 08:43:06 +0100

    added the project

---
+++ alx_travel_app/urls.py
@@ -0,0 +1,48 @@
+"""
+URL configuration for alx_travel_app project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from rest_frameworks import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# swagger schema configuartions
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ALX Travel API",
+        default_version="v1",
+        description="API Documentation for ALX Travel App Backend",
+        terms_of_service="http://example.com/terms/",
+        contact=openapi.Contact(email="support@exmaple.com"),
+        license=openapi.License(name="BSD License")
+
+        ),
+        public=True,
+        permission_classes=(permissions.AllowAny,),
+    )
+
+# urls within the app alx_travel_app
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    # include API routes
+    path("api/", incllude("alx_travel_app.urls")),
+    # swagger documentation routes
+    path("swagger/", schema_view_with_ui("swagger", cache_timeout=0), name="swaager-schema-ui"),
+    path("swagger.json", schema_view.wthout_ui(cache_timeout=0), name="schema-json"),
+    path("swagger.yaml", schema_view.wthout_ui(cache_timeout=0), name="schema-yaml"),
+]
